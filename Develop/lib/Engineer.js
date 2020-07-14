1ae72b639b10a9abb7e348c8b4d860667533f383
jeanhern81 <jeanhern81@gmail.com>
2020-07-14 12:54:14 -0700

    had a syntax error, replaced square brackets with curly braces

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,6 +1,6 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
 
-const Employee = require("../lib/Employee");
+const Employee = require("../lib/Employee.js");
 
 class Engineer extends Employee {
     constructor (name, id, email, github) {
@@ -11,9 +11,10 @@ class Engineer extends Employee {
     getGithub() {
         return(this.github);
     };
-    getRole() [
-        return "Engineer";
-    ]
+    getRole() {
+    return "Engineer";
+    }
+    
 };
 
 module.exports = Engineer;

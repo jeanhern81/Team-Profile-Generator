d167dc82f4768e5721acbcf0a71253c6f31e5d8a
jeanhern81 <jeanhern81@gmail.com>
2020-07-14 16:01:59 -0700

    added functions to app.js file

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+const Manager = require("./Develop/lib/Manager");
+const Engineer = require("./Develop/lib/Engineer.js");
+const Intern = require("./Develop/lib/Intern.js");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -8,14 +8,14 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
+const render = require("./Develop/lib/htmlRenderer");
 
 
 const team = [];
 
 //Function to prompt user to select a role
 function employeeInformation() {
-    inquirer.prompt9([
+    inquirer.prompt([
         {
             type: "list",
             message: "What employee classiction would you like to input?",
@@ -33,5 +33,111 @@ function employeeInformation() {
             generateHTML(outputPath, render(team));
         };
     });
+}
+
+//Function to colletion information on manager
+function managerInformation() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "What's your manager's name?",
+            name: "name",
+        },
+        {
+            type: "input",
+            message: "What's your manager's ID?",
+            name: "id",
+        },
+        {
+            type: "input",
+            message: "What's your manager's email?",
+            name: "email",
+        },
+        {
+            type: "input",
+            message: "What's your manager's office number?",
+            name: "number",
+        },
+    ]).then(function(answer) {
+        let manager = new Manager(answer.name, answer.id, answer.email, answer.number)
+        team.push(manager);
+
+        employeeInformation()
+    });
+}
+
+//Fucntion to collect information on engineer and after to a new employee
+function engineerInformation() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            mesasge: "What's your engienner's name?",
+            name: "name",
+        },
+        {
+            type: "input",
+            message: "What's your engineer's ID?",
+            name: "id",
+        },
+        {
+            type: "input",
+            message: "What's your engineer's email?",
+            name: "email",
+        },
+        {
+            type: "input",
+            message: "What's your engineer's GitHub username?",
+            name: "GitHub",
+        },
+    ]).then(function(answer) {
+        let engineer = new Engineer(answer.name, answer.id, answer.email, answer.GitHub)
+        team.push(engineer);
+        employeeInformation()
+    });
+}
+
+
+
+//Function to collect information on intern and then after the employee
+function internInformation() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "What's your interns name?",
+            name: "name",
+        },
+        {
+            type: "input",
+            message: "What's your intern's ID?",
+            name: "id",
+        },
+        {
+            type: "input",
+            message: "What's intern's email?",
+            name: "email",
+        },
+        {
+            type: "input",
+            message: "What's your intern's school",
+            name: "school",
+        },
+    ]).then(function(answer) {
+        let intern = new Intern(answer.name, answer.id, answer.email, answer.school)
+        team.push(intern);
+        employeeInformation ()
+
+    });
+}
+
+//Function to create README file
+function generateHTML(fileName, data) {
+    fs.writeFile(fileName, data, "utf8", function (err) {
+    if (err) {
+        throw err;
+    }
+    console.log("You have successfully written an Employee Summary");
+    });
 };
+
+employeeInformation();
 
